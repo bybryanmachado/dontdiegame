5768bfebc83bd71dc0265c3143521e3a01bf4925
Bryan Machado <bryan.machado@giant.cos.edu>
2021-03-11 18:36:43 -0800

    changed spaceship to ufo

---
+++ dontdiegame/form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Me.Hide()
+        Form2.Show()
+    End Sub
+End Class
